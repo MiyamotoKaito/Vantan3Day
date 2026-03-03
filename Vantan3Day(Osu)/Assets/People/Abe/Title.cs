a4ba4e498f049031bd322ce56e0b78558120c40d
kenkatou354 <kenkatou354@gmail.com>
2026-03-03 11:00:57 +0900

    [add] タイトルのUIをつなぐためのクラスを追加

---
+++ Vantan3Day(Osu)/Assets/People/Abe/Title.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+namespace Title
+{
+    public class Title : VisualElement
+    {
+        public new class UxmlFactory : UxmlFactory<Title, UxmlTraits>
+        {
+
+        }
+    }
+}
