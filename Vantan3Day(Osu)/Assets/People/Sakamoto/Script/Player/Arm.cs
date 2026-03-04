c691bb4414c74275bd8361f20267e1ef4758d0ef
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2026-03-04 15:31:06 +0900

    [update]item

---
+++ Vantan3Day(Osu)/Assets/People/Sakamoto/Script/Player/Arm.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Arm : MonoBehaviour
+{
+    public bool IsActive { get; private set; } = true;
+    public bool IsRightArm => _isRightArm;
+    public PlayerController PlayerController => _playerController;
+    /// <summary>
+    /// 右手か左手か。
+    /// </summary>
+    [SerializeField] private bool _isRightArm;
+    private PlayerController _playerController;
+
+    public void Init(PlayerController playerController)
+    {
+        _playerController = playerController;
+    }
+
+    public void SetActive(bool isActive)
+    {
+        IsActive = isActive;
+    }
+
+    private void Update()
+    {
+        if (_isRightArm)
+        {
+            if (_isRightArm == _playerController.IsRightHand)
+            {
+                IsActive = true;
+            }
+            else
+            {
+                IsActive = false;
+            }
+        }
+        else
+        {
+            if (_isRightArm != _playerController.IsRightHand)
+            {
+                IsActive = true;
+            }
+            else
+            {
+                IsActive = false;
+            }
+        }
+    }
+}
