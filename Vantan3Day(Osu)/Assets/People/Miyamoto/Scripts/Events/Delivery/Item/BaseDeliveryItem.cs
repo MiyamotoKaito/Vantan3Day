8e0cf9523199f65a3327ffe108972eea1e19d83a
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-03-04 11:58:08 +0900

    [update]ファイル移動

--- Vantan3Day(Osu)/Assets/People/Miyamoto/Scripts/Events/Delivery/Item/BaseDeliveryItem.cs
+++ Vantan3Day(Osu)/Assets/People/Miyamoto/Scripts/Events/Delivery/Item/BaseDeliveryItem.cs
@@ -1,16 +1,24 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 public abstract class BaseDeliveryItem : MonoBehaviour, IDelivery, IPointerClickHandler
 {
+    public event Action OnClicked;
+
     [SerializeField]
     protected DeliveryConfig _deliveryConfig;
+    [SerializeField]
+    protected GameObject _provider;
     protected Animator _animator;
+
     public abstract void GetEffect();
 
     public void OnPointerClick(PointerEventData eventData)
     {
         GetEffect();
+        OnClicked?.Invoke();
+        Destroy(gameObject);
     }
     private void Awake()
     {
