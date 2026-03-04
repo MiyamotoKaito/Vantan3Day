4a447899d4fd07d393af05312c1a583ab7d9b915
yuzu1012 <yuzusonyuzuha@gmail.com>
2026-03-04 12:50:15 +0900

    [fix]処理削除

--- Vantan3Day(Osu)/Assets/People/Yamamoto/Script/Obstruction.cs
+++ Vantan3Day(Osu)/Assets/People/Yamamoto/Script/Obstruction.cs
@@ -10,19 +10,7 @@ public abstract class Obstruction
 }
 
 //TODO：一旦、ここは無しで妨害は実装しないようにする
-//TODO：ここで全ての妨害処理を書いていく
-//TODO：宇宙人などの妨害
-
-/// <summary>
-/// 視界がぼやける
-/// </summary>
-public class VisionBecomeBlurry : Obstruction
-{
-    public override void ObstructionExecution()
-    {
-        
-    }
-}
+//TODO：ゲームオーバー以外、妨害なし
 
 /// <summary>
 /// 強制的にゲームオーバー
