b228b297f442db644db3bc0d9b15faa66965d7dc
hiaki0617 <hiaki0617@gmail.com>
2026-03-07 02:51:04 +0900

    [ひきつぎ] マージされたシーンを引き継いで、マスターシーンの整理と編集をした。

---
+++ Vantan3Day(Osu)/Assets/People/Higashi/Script/TestSound.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class TestSound : MonoBehaviour
+{
+    public void OpenSE()
+    {
+        SoundManager.Instance.PlaySE(SoundManager.SEType.ShatterOpen);
+    }
+    public void CloseSE()
+    {
+        SoundManager.Instance.PlaySE(SoundManager.SEType.ShatterClose);
+    }
+}
